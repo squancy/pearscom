41dffaf9b3337ced36badc02dfd50779896bef26
squancy <mark.frankli@pearscom.com>
2019-11-09 19:18:23 +0100

    Delete prot.php

--- prot.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $URL = $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
-    if ($URL != "www.pearscom.com/groups.php")
-    die("/\*sry, no acces rights\*/");
-?>
-<script src="/jquery_in.js"></script>
