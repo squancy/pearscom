d26cfea712a2f1625b16bec60cfc4b6a429e7391
squancy <mark.frankli@pearscom.com>
2019-11-26 22:17:08 +0100

    Update view_friends.php

--- view_friends.php
+++ view_friends.php
@@ -17,7 +17,7 @@
         header('Location: /index');
         exit();
     }
-
+    
     // This first query is just to get the total count of rows
     $sql = "SELECT COUNT(id) FROM friends WHERE user1 = ? OR user2 = ? AND accepted = ?";
     $stmt = $conn->prepare($sql);
@@ -81,26 +81,26 @@
             $paginationCtrls .= ' &nbsp; &nbsp; <a href="/view_friends/'.$u.'&pn='.$next.'&otype='.$otype.'">Next</a> ';
         }
     }
-
+    
     // Select the member from the users table
     $sql = "SELECT * FROM users WHERE username=? AND activated=? LIMIT 1";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ss",$u,$one);
     $stmt->execute();
     $result = $stmt->get_result();
-
+    
     // Now make sure the user exists in the table
     if($result->num_rows < 1){
         header('location: /usernotexist');
         exit();
     }
-
+    
     // Check to see if the viewer is the account owner
     $isOwner = "No";
     if($u == $log_username && $user_ok == true){
         $isOwner = "Yes";
     }
-?>
+    ?>
 <?php
     $otype = "sort_4";
     $friendsHTML = '';
@@ -133,7 +133,7 @@
                 array_push($all_friends, $row["user1"]);
             }
             $stmt->close();
-
+    
             $sql = "SELECT user1, user2 FROM friends WHERE user1 = ? AND accepted=? $limit";
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("ss",$u,$one);
@@ -165,7 +165,7 @@
             }else if($otype == "sort_7"){
                 $sql = "SELECT * FROM users WHERE username IN('$imp') ORDER BY country DESC $limit";
             }
-
+    
             $stmt = $conn->prepare($sql);
             if($otype == "sort_0" || $otype == "sort_1"){
                 $stmt->bind_param("s",$isOnLine);
@@ -203,9 +203,9 @@
     	        }else{
     	            $sourceURL = "style='background-image: url(\"$friend_pic\"); ".$style."' class='friendpics'";
     	        }
-
+    
                 $age = floor((time() - strtotime($bday)) / 31556926);
-
+    
                 $friendsHTML .= '<div><a href="/user/'.$friend_username.'/"><div  '.$sourceURL.'></div></a><div id="contviewf" style="width: calc(100% - 80px); margin-left: 10px;"><p><span>'.$friend_username.'<br /></span><span>'.$friend_country.'<br /></span>'.$age.' years old</p></div></div>';
             }
             $stmt->close();
@@ -215,14 +215,14 @@
             }
         }
     }
-
+    
     $sql = "SELECT COUNT(id) FROM users WHERE username IN('$imp')";
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $stmt->bind_result($countFriends);
     $stmt->fetch();
     $stmt->close();
-
+    
     $yes = "yes";
     $sql = "SELECT COUNT(id) FROM users WHERE username IN('$imp') AND online = ?";
     $stmt = $conn->prepare($sql);
@@ -236,187 +236,179 @@
     if($u == $log_username){
         $toggle = "yes";
     }
-
-?>
+    
+    ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title><?php echo $u; ?>'s all friends</title>
-    <meta charset="utf-8">
-    <link rel="icon" type="image/x-icon" href="/images/newfav.png">
-    <link rel="stylesheet" type="text/css" href="/style/style.css">
-    <meta name="description" content="Check <?php echo $u; ?>'s all friends.">
-    <script src="/js/main.js" async></script>
-    <script src="/js/ajax.js" async></script>
-    <script src="/js/dialog.js" async></script>
-    	  <link rel="manifest" href="/manifest.json">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="#282828">
-<meta name="apple-mobile-web-app-title" content="Pearscom">
-<link rel="apple-touch-icon" href="/images/icons/icon-152x152.png">
-<meta name="theme-color" content="#282828" />
-    <style type="text/css">
-        @media only screen and (max-width: 1000px){ 
-          #searchArt{
+    <head>
+        <title><?php echo $u; ?>'s all friends</title>
+        <meta charset="utf-8">
+        <link rel="icon" type="image/x-icon" href="/images/newfav.png">
+        <link rel="stylesheet" type="text/css" href="/style/style.css">
+        <meta name="description" content="Check <?php echo $u; ?>'s all friends.">
+        <script src="/js/main.js" async></script>
+        <script src="/js/ajax.js" async></script>
+        <script src="/js/dialog.js" async></script>
+        <link rel="manifest" href="/manifest.json">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#282828">
+        <meta name="apple-mobile-web-app-title" content="Pearscom">
+        <link rel="apple-touch-icon" href="/images/icons/icon-152x152.png">
+        <meta name="theme-color" content="#282828" />
+        <style type="text/css">
+            @media only screen and (max-width: 1000px){ 
+            #searchArt{
             width: 90% !important;
-          }
-
-          #artSearchBtn{
+            }
+            #artSearchBtn{
             width: 10% !important;
-          }
-
-          .longSearches{
+            }
+            .longSearches{
             width: calc(90% - 15px) !important;
             }
-
-          @media only screen and (max-width: 500px){
+            @media only screen and (max-width: 500px){
             #searchArt {
             width: 85% !important;
-          }
-
-          #artSearchBtn {
+            }
+            #artSearchBtn {
             width: 15% !important;
-          }
-
-          .longSearches{
+            }
+            .longSearches{
             width: calc(100% - 30px) !important;
             }
-        }
-    }
-    </style>
-    <script type="text/javascript">
-        let origin = "<?php echo $toggle; ?>";
-    
-        function getMyFLArr()
-            /*Scope Closed:false | writes:true*/
-            {
-                var a = _('searchArt').value;
-                if (a == '')
-                    return false;
-                var r = encodeURI(a);
-                if(origin == "no") window.location = '/search_friends/' + encodeURI(a) + "&origin=<?php echo $u; ?>";
-                else window.location = '/search_friends/' + encodeURI(a);
             }
-        var uname = '<?php echo $u; ?>';
-
-        function getFriends(e) {
-          if ("" == e) {
-            return _("frSearchResult").style.display = "none", false;
-          }
-          _("frSearchResult").style.display = "block";
-          if ("" == _("frSearchResult").innerHTML) {
-            _("frSearchResult").innerHTML = '<img src="/images/rolling.gif" width="30" height="30">';
-          }
-          var str = encodeURI(e);
-          var mypostrequest = new XMLHttpRequest;
-          mypostrequest.open("POST", "/searchn_friends.php", true);
-          mypostrequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-          mypostrequest.onreadystatechange = function() {
-            if (4 == mypostrequest.readyState && 200 == mypostrequest.status) {
-              var iconValue = mypostrequest.responseText;
-              if ("" != iconValue) {
-                _("frSearchResult").innerHTML = iconValue;
+            }
+        </style>
+        <script type="text/javascript">
+            let origin = "<?php echo $toggle; ?>";
+            
+            function getMyFLArr()
+                /*Scope Closed:false | writes:true*/
+                {
+                    var a = _('searchArt').value;
+                    if (a == '')
+                        return false;
+                    var r = encodeURI(a);
+                    if(origin == "no") window.location = '/search_friends/' + encodeURI(a) + "&origin=<?php echo $u; ?>";
+                    else window.location = '/search_friends/' + encodeURI(a);
+                }
+            var uname = '<?php echo $u; ?>';
+            
+            function getFriends(e) {
+              if ("" == e) {
+                return _("frSearchResult").style.display = "none", false;
               }
+              _("frSearchResult").style.display = "block";
+              if ("" == _("frSearchResult").innerHTML) {
+                _("frSearchResult").innerHTML = '<img src="/images/rolling.gif" width="30" height="30">';
+              }
+              var str = encodeURI(e);
+              var mypostrequest = new XMLHttpRequest;
+              mypostrequest.open("POST", "/searchn_friends.php", true);
+              mypostrequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+              mypostrequest.onreadystatechange = function() {
+                if (4 == mypostrequest.readyState && 200 == mypostrequest.status) {
+                  var iconValue = mypostrequest.responseText;
+                  if ("" != iconValue) {
+                    _("frSearchResult").innerHTML = iconValue;
+                  }
+                }
+              };
+              mypostrequest.send("u=" + str + "&imp=<?php echo $imp; ?>");
             }
-          };
-          mypostrequest.send("u=" + str + "&imp=<?php echo $imp; ?>");
-        }
-    </script>
-</head>
-<body>
-    <?php include_once("template_pageTop.php"); ?>
-    <div id="pageMiddle_2">
-        <div id="artSearch">
-            <div id="artSearchInput">
-                <input id="searchArt" type="text" autocomplete="off" onkeyup="getFriends(this.value)" placeholder="Search among friends">
-                <div id="artSearchBtn" onclick="getMyFLArr()"><img src="/images/searchnav.png" width="17" height="17"></div>
+        </script>
+    </head>
+    <body>
+        <?php include_once("template_pageTop.php"); ?>
+        <div id="pageMiddle_2">
+            <div id="artSearch">
+                <div id="artSearchInput">
+                    <input id="searchArt" type="text" autocomplete="off" onkeyup="getFriends(this.value)" placeholder="Search among friends">
+                    <div id="artSearchBtn" onclick="getMyFLArr()"><img src="/images/searchnav.png" width="17" height="17"></div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div id="frSearchResult" class="longSearches"></div>
+            <div id="data_holder">
+                <div>
+                    <div><span><?php echo $countFriends; ?></span> friends</div>
+                    <div><span><?php echo $countOFriends; ?></span> online</div>
+                </div>
+            </div>
+            <button id="sort" class="main_btn_fill">Filter Friends</button>
+            <div id="sortTypes">
+                <div class="gridDiv">
+                    <p class="mainHeading">Activity</p>
+                    <div id="sort_0">Online</div>
+                    <div id="sort_1">Offline</div>
+                </div>
+                <div class="gridDiv">
+                    <p class="mainHeading">Relation length</p>
+                    <div id="sort_2">new friends to oldest</div>
+                    <div id="sort_3">old friends to newest</div>
+                </div>
+                <div class="gridDiv">
+                    <p class="mainHeading">Username</p>
+                    <div id="sort_4">Alphabetical order</div>
+                    <div id="sort_5">Reverese alphabetical order</div>
+                </div>
+                <div class="gridDiv">
+                    <p class="mainHeading">Country</p>
+                    <div id="sort_6">Alphabetical order</div>
+                    <div id="sort_7">Reverse alphabetical order</div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="clear"></div>
+            <hr class="dim">
+            <div id="momofdif" class="flexibleSol">
+                <?php echo $friendsHTML; ?>
             </div>
             <div class="clear"></div>
         </div>
-
-        <div id="frSearchResult" class="longSearches"></div>
-
-        <div id="data_holder">
-            <div>
-                <div><span><?php echo $countFriends; ?></span> friends</div>
-                <div><span><?php echo $countOFriends; ?></span> online</div>
-            </div>
         </div>
-
-        <button id="sort" class="main_btn_fill">Filter Friends</button>
-        <div id="sortTypes">
-            <div class="gridDiv">
-                <p class="mainHeading">Activity</p>
-                <div id="sort_0">Online</div>
-                <div id="sort_1">Offline</div>
-            </div>
-            <div class="gridDiv">
-                <p class="mainHeading">Relation length</p>
-                <div id="sort_2">new friends to oldest</div>
-                <div id="sort_3">old friends to newest</div>
-            </div>
-            <div class="gridDiv">
-                <p class="mainHeading">Username</p>
-                <div id="sort_4">Alphabetical order</div>
-                <div id="sort_5">Reverese alphabetical order</div>
-            </div>
-            <div class="gridDiv">
-                <p class="mainHeading">Country</p>
-                <div id="sort_6">Alphabetical order</div>
-                <div id="sort_7">Reverse alphabetical order</div>
-            </div>
-            <div class="clear"></div>
-        </div>
-        <div class="clear"></div>
-        <hr class="dim">
-      <div id="momofdif" class="flexibleSol">
-        <?php echo $friendsHTML; ?>
-        </div>
-      <div class="clear"></div>
-    </div>
-    </div>
-    <?php require_once 'template_pageBottom.php'; ?>
-    <script type="text/javascript">
-        function getCookie(e){for(var t=e+"=",r=decodeURIComponent(document.cookie).split(";"),a=0;a<r.length;a++){for(var s=r[a];" "==s.charAt(0);)s=s.substring(1);if(0==s.indexOf(t))return s.substring(t.length,s.length)}return""}function setDark(){var e="thisClassDoesNotExist";if(!document.getElementById(e)){var t=document.getElementsByTagName("head")[0],r=document.createElement("link");r.id=e,r.rel="stylesheet",r.type="text/css",r.href="/style/dark_style.css",r.media="all",t.appendChild(r)}}var isdarkm=getCookie("isdark");"yes"==isdarkm&&setDark();
-
-        $( "#sort" ).click(function() {
-          $( "#sortTypes" ).slideToggle( 200, function() {
-            // Animation complete.
-          });
-        });
-
-    for(let i = 0; i < 8; i++){
-        addListener("sort_" + i, "sort_" + i);
-    }
-
-    function addListener(onw, w){
-        _(onw).addEventListener("click", function(){
-            _("momofdif").innerHTML = '<img src="/images/rolling.gif" width="30" height="30">';
-            filterArts(w);
-        });
-    }
-
-    function filterArts(otype){
-        changeStyle(otype);
-        let req = new XMLHttpRequest();
-        req.open("GET", "/view_friends.php?u=<?php echo $u; ?>&otype=" + otype, false);
-        req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        req.onreadystatechange = function(){
-            if(req.readyState == 4 && req.status == 200){
-                _("momofdif").innerHTML = req.responseText;
+        <?php require_once 'template_pageBottom.php'; ?>
+        <script type="text/javascript">
+            function getCookie(e){for(var t=e+"=",r=decodeURIComponent(document.cookie).split(";"),a=0;a<r.length;a++){for(var s=r[a];" "==s.charAt(0);)s=s.substring(1);if(0==s.indexOf(t))return s.substring(t.length,s.length)}return""}function setDark(){var e="thisClassDoesNotExist";if(!document.getElementById(e)){var t=document.getElementsByTagName("head")[0],r=document.createElement("link");r.id=e,r.rel="stylesheet",r.type="text/css",r.href="/style/dark_style.css",r.media="all",t.appendChild(r)}}var isdarkm=getCookie("isdark");"yes"==isdarkm&&setDark();
+            
+            $( "#sort" ).click(function() {
+              $( "#sortTypes" ).slideToggle( 200, function() {
+                // Animation complete.
+              });
+            });
+            
+            for(let i = 0; i < 8; i++){
+            addListener("sort_" + i, "sort_" + i);
             }
-        }
-        req.send();
-    }
-
-    function changeStyle(otype){
-        _(otype).style.color = "red";
-        for(let i = 0; i < 8; i++){
-            if("sort_" + i != otype) _("sort_" + i).style.color = "black";
-        }
-    }
-
-    changeStyle("sort_4");
-    </script>
-</body>
+            
+            function addListener(onw, w){
+            _(onw).addEventListener("click", function(){
+                _("momofdif").innerHTML = '<img src="/images/rolling.gif" width="30" height="30">';
+                filterArts(w);
+            });
+            }
+            
+            function filterArts(otype){
+            changeStyle(otype);
+            let req = new XMLHttpRequest();
+            req.open("GET", "/view_friends.php?u=<?php echo $u; ?>&otype=" + otype, false);
+            req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            req.onreadystatechange = function(){
+                if(req.readyState == 4 && req.status == 200){
+                    _("momofdif").innerHTML = req.responseText;
+                }
+            }
+            req.send();
+            }
+            
+            function changeStyle(otype){
+            _(otype).style.color = "red";
+            for(let i = 0; i < 8; i++){
+                if("sort_" + i != otype) _("sort_" + i).style.color = "black";
+            }
+            }
+            
+            changeStyle("sort_4");
+        </script>
+    </body>
 </html>
