a5ecc52e34d1f901ba835f50e0339e7adc4b9bf7
Mark Frankli <mark@pearscom.com>
2020-02-04 17:32:21 +0100

    comment; clean

--- art_all_exec.php
+++ art_all_exec.php
@@ -1,4 +1,8 @@
 <?php
+  /*
+    Search in all articles.
+  */
+
 	require_once 'php_includes/conn.php';
 	require_once 'timeelapsedstring.php';
 	require_once 'safe_encrypt.php';
@@ -12,29 +16,30 @@
 		$a = mysqli_real_escape_string($conn, $_POST["a"]);
 		$phpu = mysqli_real_escape_string($conn, $_POST["phpu"]);	
 		if ($a == ""){
-			// They tried to defeat our security
 			echo $output;
 			exit();		
 		}
 		$a_search = "$a%";
 		include("php_includes/conn.php");
+
 		$sql = "SELECT a.*, u.*
 				FROM articles AS a
 				LEFT JOIN users AS u
 				ON a.written_by = u.username
-		        WHERE a.written_by = ? AND (a.title LIKE ? OR a.tags LIKE ? OR a.category LIKE ?)
+		    WHERE a.written_by = ? AND (a.title LIKE ? OR a.tags LIKE ? OR a.category LIKE ?)
 				ORDER BY a.title ASC LIMIT 100";
 		$stmt = $conn->prepare($sql);
 		$stmt->bind_param("ssss",$phpu,$a_search,$a_search,$a_search);
 		$stmt->execute();
 		$result = $stmt->get_result();
+
 		if($result->num_rows > 0){
 			$output .= '';
 			while ($row = $result->fetch_assoc()){
 				$written_by = $row["written_by"];
 				$title = stripslashes($row["title"]);
-			    $title = str_replace('\'', '&#39;', $title);
-		        $title = str_replace('\'', '&#34;', $title);
+			  $title = str_replace('\'', '&#39;', $title);
+		    $title = str_replace('\'', '&#34;', $title);
 				$avatar = $row["avatar"];
 				$tags = $row["tags"];
 				$posttime = $row["post_time"];
@@ -46,13 +51,26 @@
 				$posttime = base64url_encode($posttime,$hshkey);
 				$cover = chooseCover($cat);
 				
-				$output .= "<a href='/articles/".$posttime."/".$written_by_original."' style='color: #000;'><div id='nev_rel_holder_ph_e' class='pcmacsm'>".$cover."<div id='new_inner_div_'><p style='margin-top: 10px; font-size: 14px;'><b>Author: </b>".$written_by."<br><b>Title: </b>".$title."</div></div></a>";
+				$output .= "
+          <a href='/articles/".$posttime."/".$written_by_original."' style='color: #000;'>
+            <div id='nev_rel_holder_ph_e' class='pcmacsm'>
+              ".$cover."
+              <div id='new_inner_div_'>
+                <p style='margin-top: 10px; font-size: 14px;'>
+                  <b>Author: </b>".$written_by."<br>
+                  <b>Title: </b>".$title."
+                </p>
+              </div>
+            </div>
+          </a>";
 			}
 			echo $output;
 			exit();
 		} else {
-			// No results from search
-			echo "<p style='text-align: center; margin: 0; padding: 10px; color: #999;'>Unfortunately, there are no results found</p>";
+			echo "
+      <p style='text-align: center; margin: 0; padding: 10px; color: #999;'>
+        Unfortunately, there are no results found
+      </p>";
 			exit();
 		}
 	}
