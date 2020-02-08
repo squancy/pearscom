17c80972b65d93876efd13409a1de7aaec26c236
Mark Frankli <mark@pearscom.com>
2020-02-09 00:08:40 +0100

    forgot pass test & js

---
+++ js/specific/forgot.js
@@ -0,0 +1,46 @@
+function forgotpass() {
+  var e = _("email").value;
+  if (e == "") {
+    _("status").innerHTML = `
+      <p style='font-size: 14px; color: red;'>
+        Type in your email address
+      </p>
+    `;
+    } else {
+      _("forgotpassbtn").style.display = "none";
+      _("status").innerHTML = '<img src="/images/rolling.gif" width="30" height="30">';
+      var s = ajaxObj("POST", "forgot_pass.php");
+      s.onreadystatechange = function() {
+        if (ajaxReturn(s)) {
+          var response = s.responseText;
+          _("forgotpassbtn").style.display = "block";
+          if (response == "success") {
+            _("loginform").innerHTML = `
+              <p style='font-size: 14px; color: #129c12;'>
+                Step 2. Check your email inbox in a few minutes
+              </p>
+              <p style='font-size: 14px;'>
+                You can close this window or tab if you like, every detail will be readable
+                in the email ...
+              </p>
+            `;
+          } else if (response == "no_exists") {
+            _("status").innerHTML = `
+              <p style='font-size: 14px; color: red;'>
+                Sorry that email address is not in our system
+              </p>
+            `;
+          } else if (response == "email_send_failed") {
+            _("status").innerHTML = `
+            <p style='font-size: 14px; color: red;'>
+              Unfortunately we could not send your email
+            </p>
+          `;
+          } else {
+            _("status").innerHTML = `<p style='font-size: 14px; color: red;'>${response}</p>`;
+          }
+        }
+      }
+    }
+  s.send("e=" + e);
+}
