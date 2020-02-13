3954beefd0a6a44850ed755b6a027a06e30db2c4
Mark Frankli <mark@pearscom.com>
2020-02-13 22:23:10 +0100

    js format

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-function _(e){return document.getElementById(e)}function toggleElement(e){"block"==(e=_(e)).style.display?e.style.display="none":e.style.display="block"}
+function _(el) {
+  return document.getElementById(el);
+}
+
+function toggleElement(e) {
+  "block" == (e = _(e)).style.display ? e.style.display = "none" : e.style.display = "block"
+}
