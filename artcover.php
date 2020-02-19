fac265cced4f3702382a7aae039c86c091dd025d
Mark Frankli <mark@pearscom.com>
2020-02-19 19:09:47 +0100

    indent

--- artcover.php
+++ artcover.php
@@ -1,69 +1,99 @@
 <?php
-	if($cat == "School"){
-	    $cover = '<img src="/images/art_cover/school.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Business"){
-		$cover = '<img src="/images/art_cover/business.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Learning"){
-		$cover = '<img src="/images/art_cover/learning.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "My Dreams"){
-		$cover = '<img src="/images/art_cover/dreams.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Money"){
-		$cover = '<img src="/images/art_cover/money.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Technology"){
-		$cover = '<img src="/images/art_cover/technology.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Video Games"){
-		$cover = '<img src="/images/art_cover/vgames.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "TV programmes"){
-		$cover = '<img src="/images/art_cover/tv.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Hobbies"){
-		$cover = '<img src="/images/art_cover/hobbies.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Music"){
-		$cover = '<img src="/images/art_cover/music.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Freetime"){
-		$cover = '<img src="/images/art_cover/freetime.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Travelling"){
-		$cover = '<img src="/images/art_cover/travelling.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Books"){
-		$cover = '<img src="/images/art_cover/books.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Politics"){
-		$cover = '<img src="/images/art_cover/politics.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Movies"){
-		$cover = '<img src="/images/art_cover/movies.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Lifestyle"){
-		$cover = '<img src="/images/art_cover/lifestyle.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Food"){
-		$cover = '<img src="/images/art_cover/food.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Knowledge"){
-		$cover = '<img src="/images/art_cover/knowledge.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Language"){
-		$cover = '<img src="/images/art_cover/language.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Experiences"){
-		$cover = '<img src="/images/art_cover/experiences.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Love"){
-		$cover = '<img src="/images/art_cover/love.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Recipes"){
-		$cover = '<img src="/images/art_cover/recipes.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Personal Stories"){
-		$cover = '<img src="/images/art_cover/pstories.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Product Review"){
-		$cover = '<img src="/images/art_cover/preview.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "History"){
-		$cover = '<img src="/images/art_cover/history.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Religion"){
-		$cover = '<img src="/images/art_cover/religion.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Entertaintment"){
-		$cover = '<img src="/images/art_cover/ent.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "News"){
-		$cover = '<img src="/images/art_cover/news.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Animals"){
-		$cover = '<img src="/images/art_cover/animals.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Environment"){
-		$cover = '<img src="/images/art_cover/env.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "Issues"){
-		$cover = '<img src="/images/art_cover/issues.jpg" width="100" height="100" class="cover_art">';
-	}else if($cat == "The Future"){
-		$cover = '<img src="/images/art_cover/future.jpg" width="100" height="100" class="cover_art">';
-	}else{
-	    $cover = '<img src="/images/art_cover/sports.jpg" width="100" height="100" class="cover_art">';
-	}
+  if($cat == "School"){
+    $cover = '<img src="/images/art_cover/school.jpg" width="100" height="100"
+      class="cover_art">';
+  }else if($cat == "Business"){
+    $cover = '<img src="/images/art_cover/business.jpg" width="100" height="100"
+      class="cover_art">';
+  }else if($cat == "Learning"){
+    $cover = '<img src="/images/art_cover/learning.jpg" width="100" height="100"
+      class="cover_art">';
+  }else if($cat == "My Dreams"){
+    $cover = '<img src="/images/art_cover/dreams.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Money"){
+    $cover = '<img src="/images/art_cover/money.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Technology"){
+    $cover = '<img src="/images/art_cover/technology.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Video Games"){
+    $cover = '<img src="/images/art_cover/vgames.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "TV programmes"){
+    $cover = '<img src="/images/art_cover/tv.jpg" width="100" height="100" class="cover_art">';
+  }else if($cat == "Hobbies"){
+    $cover = '<img src="/images/art_cover/hobbies.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Music"){
+    $cover = '<img src="/images/art_cover/music.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Freetime"){
+    $cover = '<img src="/images/art_cover/freetime.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Travelling"){
+    $cover = '<img src="/images/art_cover/travelling.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Books"){
+    $cover = '<img src="/images/art_cover/books.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Politics"){
+    $cover = '<img src="/images/art_cover/politics.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Movies"){
+    $cover = '<img src="/images/art_cover/movies.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Lifestyle"){
+    $cover = '<img src="/images/art_cover/lifestyle.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Food"){
+    $cover = '<img src="/images/art_cover/food.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Knowledge"){
+    $cover = '<img src="/images/art_cover/knowledge.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Language"){
+    $cover = '<img src="/images/art_cover/language.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Experiences"){
+    $cover = '<img src="/images/art_cover/experiences.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Love"){
+    $cover = '<img src="/images/art_cover/love.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Recipes"){
+    $cover = '<img src="/images/art_cover/recipes.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Personal Stories"){
+    $cover = '<img src="/images/art_cover/pstories.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Product Review"){
+    $cover = '<img src="/images/art_cover/preview.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "History"){
+    $cover = '<img src="/images/art_cover/history.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Religion"){
+    $cover = '<img src="/images/art_cover/religion.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Entertaintment"){
+    $cover = '<img src="/images/art_cover/ent.jpg" width="100" height="100" class="cover_art">';
+  }else if($cat == "News"){
+    $cover = '<img src="/images/art_cover/news.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Animals"){
+    $cover = '<img src="/images/art_cover/animals.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "Environment"){
+    $cover = '<img src="/images/art_cover/env.jpg" width="100" height="100" class="cover_art">';
+  }else if($cat == "Issues"){
+    $cover = '<img src="/images/art_cover/issues.jpg" width="100" height="100"
+    class="cover_art">';
+  }else if($cat == "The Future"){
+    $cover = '<img src="/images/art_cover/future.jpg" width="100" height="100"
+    class="cover_art">';
+  }else{
+    $cover = '<img src="/images/art_cover/sports.jpg" width="100" height="100"
+    class="cover_art">';
+  }
 ?>
