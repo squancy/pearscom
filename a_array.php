b203572082107f771863841cb63aefe486ab86b4
Mark Frankli <mark@pearscom.com>
2020-02-19 19:10:54 +0100

    format

--- a_array.php
+++ a_array.php
@@ -1,3 +1,3 @@
 <?php
-    $a_cats = array('School', 'Business', 'Learning', 'My Dreams', 'Money', 'Sports', 'Video Games', 'TV programmes', 'Hobbies', 'Music', 'Freetime', 'Travelling', 'Books', 'Politics', 'Movies', 'Lifestyle', 'Food', 'Knowledge', 'Language', 'Experiences', 'Love', 'Recipes', 'Personal Stories', 'Product Review', 'History', 'Religion', 'Entertaintment', 'News', 'Animals', 'Environment', 'Issues', 'The Future', 'Technology', 'Video Games');
+  $a_cats = array('School', 'Business', 'Learning', 'My Dreams', 'Money', 'Sports', 'Video Games', 'TV programmes', 'Hobbies', 'Music', 'Freetime', 'Travelling', 'Books', 'Politics', 'Movies', 'Lifestyle', 'Food', 'Knowledge', 'Language', 'Experiences', 'Love', 'Recipes', 'Personal Stories', 'Product Review', 'History', 'Religion', 'Entertaintment', 'News', 'Animals', 'Environment', 'Issues', 'The Future', 'Technology', 'Video Games');
 ?>
