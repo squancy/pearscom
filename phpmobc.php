0aedf6c77a0b0d2ee0aa45e89e61777f32a6bfb2
Mark Frankli <mark@pearscom.com>
2020-02-21 17:38:27 +0100

    indent

--- phpmobc.php
+++ phpmobc.php
@@ -1,4 +1,5 @@
 <?php
+  // Function for checking the user-agent platform
   $useragent=$_SERVER['HTTP_USER_AGENT'];
   function mobc(){
     if(preg_match("/(android|webos|avantgo|iphone|ipad|ipod|blackberry|iemobile|bolt|boost|cricket|docomo|fone|hiptop|mini|opera mini|kitkat|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"])){
