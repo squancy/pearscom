7b3c05ab6aeb7a752b210f9459be532e94b0a74c
Mark Frankli <mark@pearscom.com>
2020-02-23 15:13:21 +0100

    source code add

--- template_pageBottom.php
+++ template_pageBottom.php
@@ -1,15 +1,16 @@
-	<footer id="pageBottom">
-	<a href="/index" class="bottom_links">Home</a>
-	<a href="/login" class="bottom_links">Log In</a>
-	<a href="/signup" class="bottom_links">Sign Up</a>
-	<a href="/help" class="bottom_links">Help &amp; Support</a>
-	<a href="/policies" class="bottom_links">Privacy and Policy</a>
-	<a href="/jedit" class="bottom_links">JEdit</a>
-	<a href="/loquela" class="bottom_links">Loquela</a>
-	<a href="/games/inferno" class="bottom_links">Survive Inferno</a>
-	<a href="/forgot_password" class="bottom_links">Forgotten Password</a>
-	<a href="/stockai" class="bottom_links">Stock AI</a>
-	<hr class="dimw">
-	<br />
-	&copy; <?php echo date("Y"); ?> Pearscom<br><br>
+<footer id="pageBottom">
+  <a href="/index" class="bottom_links">Home</a>
+  <a href="/login" class="bottom_links">Log In</a>
+  <a href="/signup" class="bottom_links">Sign Up</a>
+  <a href="/help" class="bottom_links">Help &amp; Support</a>
+  <a href="/policies" class="bottom_links">Privacy and Policy</a>
+  <a href="/jedit" class="bottom_links">JEdit</a>
+  <a href="/loquela" class="bottom_links">Loquela</a>
+  <a href="/games/inferno" class="bottom_links">Survive Inferno</a>
+  <a href="/forgot_password" class="bottom_links">Forgotten Password</a>
+  <a href="/stockai" class="bottom_links">Stock AI</a>
+  <a href="https://www.github.com/squancy/pearscom" class="bottom_links">Source code</a>
+  <hr class="dimw">
+  <br />
+  &copy; <?php echo date("Y"); ?> Pearscom<br><br>
 </footer>
