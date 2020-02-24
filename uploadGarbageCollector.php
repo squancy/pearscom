f954a0699cadba5970d464cfd827009665b1e677
Mark Frankli <mark@pearscom.com>
2020-02-24 14:21:45 +0100

    indent

--- uploadGarbageCollector.php
+++ uploadGarbageCollector.php
@@ -1,11 +1,8 @@
 <?php
-	header('Location: /index');
-?>
-<?php
-	$dir = "tempUploads/";
-	foreach (glob($dir."*") as $file){
-		if (filemtime($file) < time() - 86400){
-			unlink($file);
-	    }
-	}
+  $dir = "tempUploads/";
+  foreach (glob($dir."*") as $file){
+    if (filemtime($file) < time() - 86400){
+      unlink($file);
+    }
+  }
 ?>
