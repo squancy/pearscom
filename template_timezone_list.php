50b0581e1299ca3bc4f2cbc54034877d50beb128
Mark Frankli <mark@pearscom.com>
2020-02-24 13:55:17 +0100

    tz clean

--- template_timezone_list.php
+++ template_timezone_list.php
@@ -1,13 +1,17 @@
 <?php
-    $tz_string .= "";
-    $timezone_identifiers = DateTimeZone::listIdentifiers();
-    for ($i=0; $i < 423; $i++) {
-        $curtz = new DateTimeZone($timezone_identifiers[$i]);
-        $datetz = new DateTime("now",$curtz);
-        $offset = $curtz->getOffset($datetz);
-        $offset = $offset/3600;
-        $offset = "GMT (".$offset.":00)";
-        $tz_string .= "<option value=".$timezone_identifiers[$i].">".$offset." ".$timezone_identifiers[$i]."</option>";
-    }
-    echo $tz_string;
+  // Display all timezones as a set of HTML <option>s
+  $tz_string .= "";
+  $timezone_identifiers = DateTimeZone::listIdentifiers();
+
+  for ($i = 0; $i < 423; $i++) {
+    $curtz = new DateTimeZone($timezone_identifiers[$i]);
+    $datetz = new DateTime("now", $curtz);
+    $offset = $curtz->getOffset($datetz);
+    $offset = $offset / 3600;
+    $offset = "GMT (".$offset.":00)";
+    $tz_string .= "<option value=".$timezone_identifiers[$i].">".$offset." ".
+      $timezone_identifiers[$i]."</option>";
+  }
+
+  echo $tz_string;
 ?>
