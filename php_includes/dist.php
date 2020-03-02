7f16216db9b15e846ebcb279e89b6f1f621b7de6
Mark Frankli <mark@pearscom.com>
2020-03-02 17:36:37 +0100

    includes style

--- php_includes/dist.php
+++ php_includes/dist.php
@@ -1,22 +1,23 @@
-<?php 	
-	function vincentyGreatCircleDistance(
-      $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 3959)
-    {
-      // convert from degrees to radians
-      $latFrom = deg2rad($latitudeFrom);
-      $lonFrom = deg2rad($longitudeFrom);
-      $latTo = deg2rad($latitudeTo);
-      $lonTo = deg2rad($longitudeTo);
-    
-      $lonDelta = $lonTo - $lonFrom;
-      $a = pow(cos($latTo) * sin($lonDelta), 2) +
-        pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
-      $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
-    
-      $angle = atan2(sqrt($a), $b);
-     
-      $number = $angle * $earthRadius;
-      $number = round($number);
-      return $number;
-    }
+<?php   
+  // Function for measuring the distance between 2 users based on their geolocation
+  function vincentyGreatCircleDistance(
+    $latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo, $earthRadius = 3959){
+
+    // convert from degrees to radians
+    $latFrom = deg2rad($latitudeFrom);
+    $lonFrom = deg2rad($longitudeFrom);
+    $latTo = deg2rad($latitudeTo);
+    $lonTo = deg2rad($longitudeTo);
+  
+    $lonDelta = $lonTo - $lonFrom;
+    $a = pow(cos($latTo) * sin($lonDelta), 2) +
+      pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+    $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+  
+    $angle = atan2(sqrt($a), $b);
+   
+    $number = $angle * $earthRadius;
+    $number = round($number);
+    return $number;
+  }
 ?>
