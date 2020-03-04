458c8f7e4ec69ba1b3982f39fefc364ad32f073a
Mark Frankli <mark@pearscom.com>
2020-03-04 16:15:04 +0100

    del oop

--- php_parsers/delete_photo.php
+++ php_parsers/delete_photo.php
@@ -1,49 +1,65 @@
 <?php
-	require_once '../php_includes/check_login_statues.php';
+  require_once '../php_includes/check_login_statues.php';
+  require_once '../php_includes/perform_checks.php';
 
-	if(isset($_POST["id"]) && $_POST["id"] != ""){
-		$id = preg_replace('#[^0-9]#', '', $_POST['id']);
-		$sql = "SELECT id FROM users WHERE username = ? LIMIT 1";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("s",$log_username);
-		$stmt->execute();
-		$res = $stmt->get_result();
-		if($res->num_rows < 1){
-			header('../index');
-			exit();
-		}
-		$stmt->close();
+  class DelPhot {
+    public function __construct($id) {
+      $this->id = preg_replace('#[^0-9]#', '', $id);
+    }
 
-		$sql = "SELECT * FROM photos WHERE id = ? AND user = ? LIMIT 1";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("is",$id,$log_username);
-		$stmt->execute();
-		$res = $stmt->get_result();
-		if($res->num_rows < 1){
-			header('../index');
-			exit();
-		}
-		$stmt->close();
+    public function photoExists($conn, $log_username) {
+      $sql = "SELECT id FROM photos WHERE id = ? AND user = ? LIMIT 1";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("is", $this->id, $log_username);
+      $stmt->execute();
+      $res = $stmt->get_result();
+      if($res->num_rows < 1){
+        echo "video does not exist";
+        exit();
+      }
+      $stmt->close();
+    }
 
-		$sql = "DELETE FROM photos WHERE id = ? AND user = ? LIMIT 1";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("is",$id,$log_username);
-		$stmt->execute();
-		$stmt->close();
+    public function deletePhoto($conn, $log_username) {
+      $sql = "DELETE FROM photos WHERE id = ? AND user = ? LIMIT 1";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("is", $this->id, $log_username);
+      $stmt->execute();
+      $stmt->close();
+    }
 
-		// Make sure it is deleted
-		$sql = "SELECT id FROM photos WHERE id = ? AND user = ? LIMIT 1";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("is",$id,$log_username);
-		$stmt->execute();
-		$res = $stmt->get_result();
-		if($res->num_rows < 1){
-			echo "delete_photo_success";
-			exit();
-		}else{
-			echo "Unfortunately an unknown error has occured. Please try again later!";
-			exit();
-		}
-		exit();
-	}
+    public function makeSure($conn, $log_username) {
+      $sql = "SELECT id FROM photos WHERE id = ? AND user = ? LIMIT 1";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("is", $this->id, $log_username);
+      $stmt->execute();
+      $res = $stmt->get_result();
+      if($res->num_rows < 1){
+        echo "delete_photo_success";
+        exit();
+      }else{
+        echo "Unfortunately an unknown error has occured. Please try again later!";
+        exit();
+      }
+      $stmt->close();
+    }
+  }
+
+  if(isset($_POST["id"]) && $_POST["id"] != ""){
+    $delP = new DelPhot($_POST['id']);
+
+    // Make sure user exists in db
+    userExists($conn, $log_username);
+
+    // Make sure photo exists
+    $delP->photoExists($conn, $log_username);
+
+    // Delete photo
+    $delP->deletePhoto($conn, $log_username);
+
+    // Make sure it is deleted
+    $delP->makeSure($conn, $log_username);
+    
+    exit();
+  }
 ?>
