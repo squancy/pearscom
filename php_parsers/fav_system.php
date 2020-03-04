ab504517b22e32cc84ab29e9b554b0027917e561
Mark Frankli <mark@pearscom.com>
2020-03-04 21:24:02 +0100

    oop fav

--- php_parsers/fav_system.php
+++ php_parsers/fav_system.php
@@ -1,12 +1,12 @@
 <?php
-	// Check to see if the user is not logged in
-	require_once '../php_includes/check_login_statues.php';
-	require_once '../php_includes/perform_checks.php';
-	require_once '../php_includes/sentToFols.php';
+  // Check to see if the user is not logged in
+  require_once '../php_includes/check_login_statues.php';
+  require_once '../php_includes/perform_checks.php';
+  require_once '../php_includes/sentToFols.php';
 
-	if(!$user_ok || !$log_username) {
-		exit();
-	}
+  if(!$user_ok || !$log_username) {
+    exit();
+  }
 
   class FollowHandler {
     public function __construct($conn, $p, $u) {
@@ -17,11 +17,11 @@
     public function alreadyFaved($conn, $log_username) {
       $sql = "SELECT COUNT(id) FROM fav_art WHERE username=? AND art_time=? AND art_uname=?
         LIMIT 1";
-			$stmt = $conn->prepare($sql);
-			$stmt->bind_param("sss", $log_username, $this->p, $this->u);
-			$stmt->execute();
-			$stmt->bind_result($row_count1);
-			$stmt->fetch();
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("sss", $log_username, $this->p, $this->u);
+      $stmt->execute();
+      $stmt->bind_result($row_count1);
+      $stmt->fetch();
       return $row_count1;
     }
 
@@ -43,21 +43,21 @@
     }
   }
 
-	if(isset($_POST['type']) && isset($_POST['p']) && isset($_POST['u'])){
+  if(isset($_POST['type']) && isset($_POST['p']) && isset($_POST['u'])){
     $folObj = new FollowHandler($conn, $_POST['p'], $_POST['u']);
     
-		// Make sure the user exists in the database
+    // Make sure the user exists in the database
     userExists($conn, $log_username);
 
-		if($_POST['type'] == "fav"){
+    if($_POST['type'] == "fav"){
       $row_count1 = $folObj->alreadyFaved($conn, $log_username);
 
-			// Check to see the user has already liked it
-			if($row_count1 > 0){
-				echo "You have already added as a favourite";
-				exit();
-			}else{
-				// Insert into database
+      // Check to see the user has already liked it
+      if($row_count1 > 0){
+        echo "You have already added as a favourite";
+        exit();
+      }else{
+        // Insert into database
         $folObj->insertFav($conn, $log_username);
 
         // Notif send
@@ -69,17 +69,17 @@
 
         $sendNotif->sendNotif($log_username, $app, $note, $conn);
 
-				mysqli_close($conn);
-				echo "fav_success";
-				exit();
-			}
-		}else if($_POST['type'] == "unfav"){
+        mysqli_close($conn);
+        echo "fav_success";
+        exit();
+      }
+    }else if($_POST['type'] == "unfav"){
       // Make sure already faved
       $row_count1 = $folObj->alreadyFaved($conn, $log_username);
 
-			if($row_count1 > 0){
+      if($row_count1 > 0){
         $folObj->delFav($conn, $log_username);
-				mysqli_close($conn);
+        mysqli_close($conn);
         echo "unfav_success";
         exit();
       }else{
@@ -87,6 +87,6 @@
         echo "You do not favourite this post";
         exit();
       }
-		}
-	}
+    }
+  }
 ?>
