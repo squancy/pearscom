69bc1ef1cf1774b25bcb86053752cf2fc3899a8b
Mark Frankli <mark@pearscom.com>
2020-03-07 22:18:07 +0100

    login oop & fix

--- php_parsers/login_parse.php
+++ php_parsers/login_parse.php
@@ -1,131 +1,147 @@
 <?php
-	require_once '../sec_session_start.php';
-	$domain = "pearscom.com";
-	sec_session_start();
-	$one = "1";
-	$yes = "yes";
-	// AJAX CALLS THIS LOGIN CODE TO EXECUTE
-	if(isset($_POST["e"])){
-		// START Expansion
-	// Get user ip address
-	$ip = preg_replace('#[^0-9.]#', '', getenv('REMOTE_ADDR'));
-	// Get referer from header
-	$refer = preg_replace('#[^a-z0-9 -._]#i', '.', getenv('HTTP_REFERER'));	
-	// Set variable for possible logging
-	$csrf = "";
-	// Check for login session	
-	if(isset($_SESSION['login']) && isset($_SESSION['login']['tm']) && isset($_SESSION['login']['tk']) && isset($_POST['t'])){
-		// Sanitize everything now
-		$sTimestamp = preg_replace('#[^0-9]#', '', $_SESSION['login']['tm']);
-		$sToken = preg_replace('#[^a-z0-9.-]#i', '', $_SESSION['login']['tk']);
-		$fToken = preg_replace('#[^a-z0-9.-]#i', '', $_POST['t']);
-		// Make sure we have values after sanitizing
-		if($sTimestamp != "" && $sToken != "" && $fToken != ""){
-			// Check if session and post token match
-			if($fToken !== $sToken){
-				$csrf .= "Form token and session token do not match|";
-			}
-			// Do 5 minute check
-			$elapsed = time() - $sTimestamp;
-			if($elapsed > 300){
-				$csrf .= "Expired session|";
-			}
-			// add more checks here if needed			
-		} else {
-			$csrf .= "A critical session or form token post was empty after sanitization|";
-		}	
-	} else {
-		// Something fishy is going on .. our session is not set
-		$csrf .= "A critical session or form token post was not set|";		
-	}
-	// CONNECT TO THE DATABASE
-	include_once("../php_includes/conn.php");
-	
-	// Check our errors here
-	if($csrf !== ""){
-		// At least one of our tests above was failed
-		// Sanitize the e & p posts for logging
-		$e = mysqli_real_escape_string($conn, $_POST['e']);
-		$p = mysqli_real_escape_string($conn, $_POST['p']);
-		// Time to log this
-		$sql = "INSERT INTO logging (dt, ip, referer, issues, epost, ppost)       
-		        VALUES(NOW(),?,?,?,?,?)";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("issss",$ip,$refer,$csrf,$e,$p);
-		$stmt->execute();
-		$stmt->close();
-		mysqli_close($conn);
-		// Unset 
-		if(isset($_SESSION['login'])){
-			unset($_SESSION['login']);
-		}
-		// Throttle back the attack
-		sleep(3);
-		// Return generic login_failed and exit script
-		echo "login_failed";
+  require_once '../sec_session_start.php';
+  require_once '../php_includes/conn.php';
+
+  $domain = "pearscom.com";
+  sec_session_start();
+
+  class LoginHandler {
+    public function __construct($tstamp1, $tstamp2, $tstamp3) {
+      $this->sTimestamp = preg_replace('#[^0-9]#', '', $tstamp1);
+      $this->sToken = preg_replace('#[^a-z0-9.-]#i', '', $tstamp2);
+      $this->fToken = preg_replace('#[^a-z0-9.-]#i', '', $tstamp3);
+      $this->csrf = '';
+    }
+
+    public function checkTimestamp() {
+      if($this->sTimestamp && $this->sToken && $this->fToken){
+        // Check if session and post token match
+        if($this->fToken !== $this->sToken){
+          $this->csrf .= "Form token and session token do not match|";
+        }
+
+        // Do 5 minute check
+        $elapsed = time() - $this->sTimestamp;
+        if($elapsed > 300){
+          $this->csrf .= "Expired session|";
+        }
+      } else {
+        $this->csrf .= "A critical session or form token post was empty after sanitization|";
+      }
+    }
+
+    public function getUserInfo($conn, $e) {
+      $one = '1';
+      $sql = "SELECT id, username, password FROM users WHERE email=? AND activated=? LIMIT 1";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("ss", $e, $one);
+      $stmt->execute();
+      $result = $stmt->get_result();
+      if($row = $result->fetch_assoc()){
+        $this->db_id = $row["id"];
+        $this->db_username = $row["username"];
+        $this->db_pass_str = $row["password"];
+      }
+      $stmt->close();
+    }
+
+    public function setCookieData() {
+      $_SESSION['userid'] = $this->db_id;
+      $_SESSION['username'] = $this->db_username;
+      $_SESSION['password'] = $this->db_pass_str;
+      setcookie("id", $this->db_id, strtotime( '+30 days' ), "/", "", "", TRUE);
+      setcookie("user", $this->db_username, strtotime( '+30 days' ), "/", "", TRUE, TRUE);
+      setcookie("pass", $this->db_pass_str, strtotime( '+30 days' ), "/", "", TRUE, TRUE);
+      echo $_COOKIE['pass'];
+      session_regenerate_id();
+    }
+
+    public function updateDb($conn, $ip) {
+      $yes = 'yes';
+      $sql = "UPDATE users SET ip=?, online=?, lastlogin=NOW() WHERE username=? LIMIT 1";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("iss", $ip, $yes, $this->db_username);
+      $stmt->execute();
+      $stmt->close();
+    }
+  }
+
+  function recordLogin($conn, $ip, $refer, $csrf, $e, $p) {
+    $sql = "INSERT INTO logging (dt, ip, referer, issues, epost, ppost)       
+            VALUES(NOW(),?,?,?,?,?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("issss", $ip, $refer, $csrf, $e, $p);
+    $stmt->execute();
+    $stmt->close();
+    mysqli_close($conn);
+  }
+
+  if(isset($_POST["e"])){
+    // Get user ip address
+    $ip = preg_replace('#[^0-9.]#', '', getenv('REMOTE_ADDR'));
+
+    // Get referer from header
+    $refer = preg_replace('#[^a-z0-9 -._]#i', '.', getenv('HTTP_REFERER'));  
+
+    // Set variable for possible logging
+    $csrf = "";
+
+    // Check for login session  
+    if(isset($_SESSION['login']) && isset($_SESSION['login']['tm']) &&
+      isset($_SESSION['login']['tk']) && isset($_POST['t'])){
+      $stamp = new LoginHandler($_SESSION['login']['tm'], $_SESSION['login']['tk'],
+        $_POST['t']);
+
+      $stamp->checkTimestamp();
+    } else {
+      $csrf .= "A critical session or form token post was not set|";    
+    }
+    
+    if($csrf || $stamp->csrf){
+      $e = mysqli_real_escape_string($conn, $_POST['e']);
+      $p = mysqli_real_escape_string($conn, $_POST['p']);
+
+      // Time to log this
+      recordLogin($conn, $ip, $refer, $csrf, $e, $p);
+
+      if(isset($_SESSION['login'])){
+        unset($_SESSION['login']);
+      }
+
+      // Throttle back the attack
+      sleep(3);
+      echo $csrf.$stamp->csrf;
+      echo "login_failed .";
+      exit();
+    }
+    
+    $e = mysqli_real_escape_string($conn, $_POST['e']);
+    $p = $_POST['p'];
+
+    if(!$e || !$p){
+      echo "login_failed ? ";
+      exit();
+    } else {
+      $stamp->getUserInfo($conn, $e);
+
+      if(!password_verify($p, $stamp->db_pass_str)){
+        echo "login_failed";
         exit();
-	}
-	
-	// Move ip grabber to top of this script
-	// Change database connection
-	// Move database connection into this script
-	// Add session unset in existing form processing if they log in	
-	// END Expansion
+      } else {
+        $stamp->setCookieData();
 
-		// GATHER THE POSTED DATA INTO LOCAL VARIABLES AND SANITIZE
-		$e = mysqli_real_escape_string($conn, $_POST['e']);
-		$p = $_POST['p'];
+        // Update info in db
+        $stamp->updateDb($conn, $ip);
 
-		// GET USER IP ADDRESS
-	    // $ip = preg_replace('#[^0-9.]#', '', getenv('REMOTE_ADDR'));
-		// FORM DATA ERROR HANDLING
-		if($e == "" || $p == ""){
-			echo "login_failed";
-	        exit();
-		} else {
-		// END FORM DATA ERROR HANDLING
-			$db_id = "";
-			$db_username = "";
-			$db_pass_str = "";
-			$sql = "SELECT id, username, password FROM users WHERE email=? AND activated=? LIMIT 1";
-	        $stmt = $conn->prepare($sql);
-	        $stmt->bind_param("ss",$e,$one);
-	        $stmt->execute();
-	        $result = $stmt->get_result();
-	        while($row = $result->fetch_assoc()){
-	        	$db_id = $row["id"];
-				$db_username = $row["username"];
-	       	 	$db_pass_str = $row["password"];
-	        }
-	        $stmt->close();
-			if(!password_verify($p, $db_pass_str)){
-				echo "login_failed";
-	            exit();
-			} else {
-				// CREATE THEIR SESSIONS AND COOKIES
-				$_SESSION['userid'] = $db_id;
-				$_SESSION['username'] = $db_username;
-				$_SESSION['password'] = $db_pass_str;
-				setcookie("id", $db_id, strtotime( '+30 days' ), "/", "", "", TRUE);
-				setcookie("user", $db_username, strtotime( '+30 days' ), "/", "", TRUE, TRUE);
-	    		setcookie("pass", $db_pass_str, strtotime( '+30 days' ), "/", "", TRUE, TRUE);
-	    		session_regenerate_id();
-				// UPDATE THEIR "IP" AND "LASTLOGIN" FIELDS
-				$sql = "UPDATE users SET ip=?, online=?, lastlogin=NOW() WHERE username=? LIMIT 1";
-	            $stmt = $conn->prepare($sql);
-	            $stmt->bind_param("iss",$ip,$yes,$db_username);
-	            $stmt->execute();
-	            $stmt->close();
+        // Unset that session if they logged in
+        if(isset($_SESSION['login'])){
+          unset($_SESSION['login']);
+        }
 
-	            // Unset that session if they logged in
-	            if(isset($_SESSION['login'])){
-	            	unset($_SESSION['login']);
-	            }
-
-				echo $db_username;
-			    exit();
-			}
-		}
-		exit();
-	}
+        echo $stamp->db_username;
+        exit();
+      }
+    }
+    exit();
+  }
 ?>
