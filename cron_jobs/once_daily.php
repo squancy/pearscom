692f7ad960bc18c17e35ab18ef7d51331c17d8b7
Mark Frankli <mark@pearscom.com>
2020-03-14 00:04:04 +0100

    indent

--- cron_jobs/once_daily.php
+++ cron_jobs/once_daily.php
@@ -1,36 +1,37 @@
 <?php
-	require_once '../php_includes/conn.php';
+  require_once '../php_includes/conn.php';
 
-	// This block will delete all accounts that do not activate after 3 days
-	$var = 0;
-	$sql = "SELECT id, username FROM users WHERE signup < CURRENT_DATE - INTERVAL 3 DAY
+  // This block will delete all accounts that do not activate after 3 days
+  $var = 0;
+  $sql = "SELECT id, username FROM users WHERE signup < CURRENT_DATE - INTERVAL 3 DAY
     AND activated=?";
-	$stmt = $conn->prepare($sql);
-	$stmt->bind_result("i",$var);
-	$stmt->execute();
-	$numrows = $stmt->num_rows;
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_result("i",$var);
+  $stmt->execute();
+  $numrows = $stmt->num_rows;
 
-	// Check to see if there any user
-	if($numrows > 0){
-		while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
-			$id = $row["id"];
-			$username = $row["username"];
-			$userFolder = "../user/$username";
-			if(is_dir($userFolder)){
-				rmdir($userFolder);
-			}
+  // Check to see if there any user
+  if($numrows > 0){
+    while ($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
+      $id = $row["id"];
+      $username = $row["username"];
+      $userFolder = "../user/$username";
+      if(is_dir($userFolder)){
+        rmdir($userFolder);
+      }
 
-			// Delete the user from the database
-			$sql_2 = "DELETE FROM users WHERE id=? AND username=? LIMIT 1";
-			$stmt = $conn->prepare($sql_2);
-			$stmt->bind_param("is", $id, $username);
-			$stmt->execute();
-			$stmt->close();
-			$sql_3 = "DELETE FROM useroptions WHERE username=? LIMIT 1";
-			$stmt = $conn->prepare($sql_3);
-			$stmt->bind_param("s", $username);
-			$stmt->execute();
-			$stmt->close();
-		}
-	}
+      // Delete the user from the database
+      $sql_2 = "DELETE FROM users WHERE id=? AND username=? LIMIT 1";
+      $stmt = $conn->prepare($sql_2);
+      $stmt->bind_param("is", $id, $username);
+      $stmt->execute();
+      $stmt->close();
+
+      $sql_3 = "DELETE FROM useroptions WHERE username=? LIMIT 1";
+      $stmt = $conn->prepare($sql_3);
+      $stmt->bind_param("s", $username);
+      $stmt->execute();
+      $stmt->close();
+    }
+  }
 ?>
--- cron_jobs/pm_trashman.php
+++ cron_jobs/pm_trashman.php
@@ -1,34 +1,35 @@
 <?php
-	require_once 'php_includes/conn.php';
-	// Private Message Database Trashman Corn Job
-	// Delete when receiver has never replied and has deleted
-	$query = mysqli_query($conn, "SELECT id FROM pm WHERE parent='x' AND rdelete='1' AND
-    hasreplies='0'");
-	$numrows = mysqli_num_rows($conn, $sql);
-	if($numrows > 0){
-		while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)){
-			$id = $row["id"];
-			$query2 = mysqli_query($conn, "DELETE FROM pm WHERE id='$id'");
-		}
-	}
+  require_once 'php_includes/conn.php';
 
-	// Delete when both users have checked delete, also delete replies
-	$query3 = mysqli_query($conn, "SELECT id FROM pm WHERE parent='x' AND sdelete='1' AND
+  // Private Message Database Trashman Corn Job
+  // Delete when receiver has never replied and has deleted
+  $query = mysqli_query($conn, "SELECT id FROM pm WHERE parent='x' AND rdelete='1' AND
+    hasreplies='0'");
+  $numrows = mysqli_num_rows($conn, $sql);
+  if($numrows > 0){
+    while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)){
+      $id = $row["id"];
+      $query2 = mysqli_query($conn, "DELETE FROM pm WHERE id='$id'");
+    }
+  }
+
+  // Delete when both users have checked delete, also delete replies
+  $query3 = mysqli_query($conn, "SELECT id FROM pm WHERE parent='x' AND sdelete='1' AND
     rdelete='1'");
-	$numrows3 = mysqli_num_rows($query3);
-	if($numrows3 > 0){
-		while($row3 = mysqli_fetch_array($query3, MYSQLI_ASSOC)){
-			$id3 = $row3["id"];
-			$query4 = mysqli_query($conn, "DELETE FROM pm WHERE id='$id3'");
-			// Gather list of replies and Delete them
-			$query5 = mysqli_query($conn, "SELECT id FROM pm WHERE parent='$id3'");
-			$numrows5 = mysqli_num_rows($query5);
-			if($numrows5 > 0){
-				while($row5 = mysqli_fetch_array($query, MYSQLI_ASSOC)){
-					$id5 = $row5["id"];
-					$query6 = mysqli_query($conn, "DELETE FROM pm WHERE id='$id5'");
-				}
-			}
-		}
-	}
+  $numrows3 = mysqli_num_rows($query3);
+  if($numrows3 > 0){
+    while($row3 = mysqli_fetch_array($query3, MYSQLI_ASSOC)){
+      $id3 = $row3["id"];
+      $query4 = mysqli_query($conn, "DELETE FROM pm WHERE id='$id3'");
+      // Gather list of replies and Delete them
+      $query5 = mysqli_query($conn, "SELECT id FROM pm WHERE parent='$id3'");
+      $numrows5 = mysqli_num_rows($query5);
+      if($numrows5 > 0){
+        while($row5 = mysqli_fetch_array($query, MYSQLI_ASSOC)){
+          $id5 = $row5["id"];
+          $query6 = mysqli_query($conn, "DELETE FROM pm WHERE id='$id5'");
+        }
+      }
+    }
+  }
 ?>
